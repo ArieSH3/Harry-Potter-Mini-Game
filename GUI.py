ee0c9d836b568699b2a64b0ca59b25dbb0023ebc
ArieSH3 <StepDubUp@gmail.com>
2021-06-04 17:03:17 +0200

    Creating initial files with proper description of their future function

--- GUI.py
+++ GUI.py
@@ -1,1 +1,12 @@
-GUI.py
+'''
+	Author: Stefan Popovic
+	Date  : 04/06/2021
+
+	About : A GUI for Harry Potter Mini Game
+
+	TODO  :
+		- Simple GUI to have basic information like inventory, health bar, map...
+		- 
+
+
+'''
---
+++ Tile_Map.py
@@ -0,0 +1,12 @@
+'''
+	Author: Stefan Popovic
+	Date  : 04/06/2021
+
+	About : Map grid/world for player to move in. 
+			Goal is to look similar to Nintendos Pokemon and Zelda game with different biomes
+			and structures to explore (Dark Forest, Hogwarts, Hogsmead ans such)
+	
+	TODO  : 
+		- Figure out how to set up different sprite blocks to create sensible terrain
+		-
+'''
